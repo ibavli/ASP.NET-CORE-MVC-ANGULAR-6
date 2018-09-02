dfd44b21c04b851739c0876287eba42a10e4c45f
ibavli <ibrahim.bavli35@gmail.com>
2018-09-03 00:55:33 +0300

    AuthRepository - Registeer

---
+++ CityGuide.API/Data/AuthRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityGuide.API.Models;
+
+namespace CityGuide.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private DatabaseContext _db;
+
+        public AuthRepository(DatabaseContext db)
+        {
+            _db = db;
+        }
+
+        public Task<User> Login(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _db.Users.AddAsync(user);
+            await _db.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac=new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public Task<bool> UserExists(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
