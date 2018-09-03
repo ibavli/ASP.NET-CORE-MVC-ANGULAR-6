2f572a0a07a39e7618d124012e3316f028fc074a
ibavli <ibrahim.bavli35@gmail.com>
2018-09-04 02:15:11 +0300

    Login and Token created

--- CityGuide.API/Controllers/AuthController.cs
+++ CityGuide.API/Controllers/AuthController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using CityGuide.API.Data;
 using CityGuide.API.Dtos;
@@ -8,6 +11,7 @@ using CityGuide.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CityGuide.API.Controllers
 {
@@ -24,6 +28,7 @@ namespace CityGuide.API.Controllers
             _configuration = configuration;
         }
 
+        [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
         {
             if (await _authRepository.UserExists(userForRegisterDto.UserName))
@@ -42,7 +47,36 @@ namespace CityGuide.API.Controllers
             };
 
             var createdUser = await _authRepository.Register(userToCreate, userForRegisterDto.Password);
-            return StatusCode(201, createdUser);
+            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
+        {
+            var user = await _authRepository.Login(userForLoginDto.UserName, userForLoginDto.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Token").Value);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]{
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.UserName)
+                }),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(tokenString);
         }
     }
 }
